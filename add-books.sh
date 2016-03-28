b33ed601730f85c6b9b306b59e86424ddb601531
Bradley Leonard <bradley@stygianresearch.com>
2016-03-28 16:52:07 +0000

    modified script to it will be able to determine if another copy is running.

--- add-books.sh
+++ add-books.sh
@@ -4,7 +4,17 @@ DIRNAME=/data/addbooks
 LIBNAME=/data/library
 CALIBREDB=/usr/bin/calibredb
 CAL_OPT="add -1 -r"
+STATUS_FILE=/data/add-books.is.running
 export LANG=en_US.UTF-8
+
+if [ -f ${STATUS_FILE} ]
+then
+  echo "${0} is curently running."
+  echo "${STATUS_FILE} exists."
+  exit 0
+else
+  cp /dev/null ${STATUS_FILE}
+fi
 
 ${CALIBREDB} ${CAL_OPT} --with-library=${LIBNAME} ${DIRNAME} > /dev/null 2>&1
 EC=$?
@@ -12,3 +22,9 @@ if [ $EC -eq 0 ]
 then
   /bin/rm -rf -- ${DIRNAME}/* > /dev/null 2>&1
 fi
+
+if [ -f ${STATUS_FILE} ]
+then
+  rm ${STATUS_FILE}
+fi
+
